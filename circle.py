1d728b248194f9ee462fe86f742d2ef142a8b179
Tanja Gehr <tanja.gehr@stud.uni-regensburg.de>
2021-04-18 19:28:50 +0200

    circle and stats done

--- circle.py
+++ circle.py
@@ -1,1 +1,29 @@
+#!/usr/bin/python3
 
+import turtle
+import sys
+import math
+import colorsys
+
+radius = float(sys.argv[1])
+x = 0
+y = 0
+
+turtle.width(8)
+
+# go to starting point
+turtle.up()
+start_x = x + radius * math.cos(0)
+start_y = y - radius * math.sin(0)
+turtle.setpos(start_x, start_y)
+turtle.down()
+
+# draw circle
+for i in range(0, 1000):
+    alpha = 2*math.pi*i/1000
+    new_x = x + radius * math.cos(alpha)
+    new_y = y - radius * math.sin(alpha)
+    turtle.goto(new_x, new_y)
+    turtle.pencolor(colorsys.hsv_to_rgb(i / 1000, 0.75, 0.75))
+
+turtle.Screen().exitonclick()
--- stats.py
+++ stats.py
@@ -1,1 +1,48 @@
+import sys
+import math
 
+content = ""
+
+# file or stdin?
+if len(sys.argv) > 1:
+    # read from file
+    filename = sys.argv[1]
+    content = open(filename).read()
+else:
+    # read from stdin
+    print("Provide numbers:")
+    content = sys.stdin.readline()
+
+# calculate mean, median and standard deviation
+# convert list into float numbers
+content = (str(content).replace(",", ".")).split()
+float_list = list()
+for i in content:
+    float_list.append(float(i))
+
+length = len(float_list)
+
+# mean
+sums = 0
+for i in float_list:
+    sums = sums + i
+mean = sums / length
+
+# median
+float_list.sort()
+if length % 2 == 0:
+    index = int(length / 2)
+    median = (float_list[index - 1] + float_list[index]) / 2
+else:
+    index = int(length/2)
+    median = float_list[index]
+
+# standard deviation
+numerator = 0
+for i in float_list:
+    sq = (i - mean) * (i - mean)
+    numerator = numerator + sq
+st_dev = math.sqrt(numerator / (length - 1))
+
+# output
+sys.stdout.write("Mean: " + str(mean) + ", Median: " + str(median) + ", Standard deviation: " + str(st_dev))
